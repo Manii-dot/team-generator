d5a658e230a9c266eeb6d8416a9a5b315eeaad0d
Manii-Dot <emmanuel.stephen68@gmail.com>
2020-10-19 20:48:23 -0500

    input info for both manager and intern

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,20 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+
+let Employee = require("./Employee");
+class Manager extends Employee {
+    constructor(name, id, email,school){
+        super(name, id, email);
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.school = school;
+    }
+    getSchool(){
+        return this.school;
+    }
+    getRole(){
+        return "Manager";
+    }
+}
+
+module.exports = Manager;
